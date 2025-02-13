d636543ddd74662df122608abd99760aeb5e61a9
Arthur Barata <arthurpbarata@gmail.com>
2025-02-12 23:17:02 -0300

    generate adaptation and clear remote state

--- proxy_generator/methods/generator.py
+++ proxy_generator/methods/generator.py
@@ -1,4 +1,9 @@
-
+RETURN_TYPES = {
+    "VOID": "void",
+    "STR": "String",
+    "INT": "int",
+    "CHAR": "char"
+}
 
 class MethodsGenerator:
     def __init__(self, methods, interface_name, attributes):
@@ -22,23 +27,24 @@ class MethodsGenerator:
             file.write(") {\n")
 
             # if has argument should distribute the argument
-            if method_props["operation"] == "write":
-                if 'arguments' in method_props:
-                    for i, arg in enumerate(method_props['arguments']):
-                        self.provide_metadata_for_write_method(file, i, method, arg['name'])
-                        attribute_affected = self.attributes[arg["connectsTo"]]
-                        operation_name = method_props["operation"].capitalize()
-                        strategy_method_name = "{}{}".format(attribute_affected["strategy"], operation_name)
-                        file.write("\t\t")
-                        self.provide_strategy_call_for_order(file, i, strategy_method_name)
+            # if method_props["operation"] == "write":
+            if 'arguments' in method_props:
+                for i, arg in enumerate(method_props['arguments']):
+                    self.provide_metadata_for_write_method(file, i, method, arg['name'])
+                    attribute_affected = self.attributes[arg["connectsTo"]]
+                    operation_name = method_props["operation"].capitalize()
+                    strategy_method_name = "{}{}".format(attribute_affected["strategy"], operation_name)
+                    file.write("\t\t")
+                    self.provide_strategy_call_for_order(file, i, strategy_method_name)
             else:
                 self.provide_metadata_for_read_method(file, i, method)
                 attribute_affected = self.attributes[method_props["connectsTo"]]
                 operation_name = method_props["operation"].capitalize()
                 strategy_method_name = "{}{}".format(attribute_affected["strategy"], operation_name)
-                file.write("\t\tResponse res = ")
+                if method_props['returnType'] != RETURN_TYPES["VOID"]: file.write("\t\tResponse res = ")
+                else: file.write("\t\t")
                 self.provide_strategy_call_for_order(file, i, strategy_method_name, method_props["returnType"])
-                file.write("\t\treturn res.d\n") # verificar se funciona
+                if method_props['returnType'] != RETURN_TYPES["VOID"]: file.write("\t\treturn res.d\n") # verificar se funciona
                 #todo: adicionar conversão de dado do response para tipo de saída da função e retornar o dado
 
             file.write("\t}\n")
@@ -51,7 +57,7 @@ class MethodsGenerator:
         file.write("\t\tRequest req{} = new Request(buildMetaForMethod(\"{}\"))\n".format(order, method_name))
     
     def provide_strategy_call_for_order(self, file, order, strategy, returnType=None):
-        if returnType is None: file.write("{}(req{})\n".format(strategy, order))
+        if returnType is None or returnType == RETURN_TYPES["VOID"]: file.write("{}(req{})\n".format(strategy, order))
         else: file.write("{}(req{}, typeof({}))\n".format(strategy, order, returnType))
 
     def provide_metadata_factory(self, file):
