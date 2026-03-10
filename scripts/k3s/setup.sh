ef17233ea9ce7714b7685dd399a313b9f2a89ff4
Arthur Barata <arthurpbarata@gmail.com>
2026-03-10 11:00:30 -0300

    update code for k3s

--- scripts/k3s/setup.sh
+++ scripts/k3s/setup.sh
@@ -1,3 +1,13 @@
+DIRECTORY="sds-proxy-generator"
+
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Directory $DIRECTORY does not exist. Creating it now..."
+    git clone https://github.com/baratarthur/sds-proxy-generator.git
+    mkdir "$DIRECTORY"
+fi
+
+source ./scripts/build/compile.sh
+
 source ./scripts/k3s/build-publish-main.sh
 source ./scripts/k3s/build-publish-remote.sh
 
