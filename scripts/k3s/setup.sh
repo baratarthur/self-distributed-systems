262aa6e626a222825cd5785896d02433139cf350
Arthur Barata <arthurpbarata@gmail.com>
2026-04-23 09:46:40 -0300

    remove library change due to dana update

--- scripts/k3s/setup.sh
+++ scripts/k3s/setup.sh
@@ -1,5 +1,5 @@
 DIRECTORY_PROXY="sds-proxy-generator"
-DIRECTORY_NATIVE_LIBRARIES="native_libraries"
+# DIRECTORY_NATIVE_LIBRARIES="native_libraries"
 
 if [ ! -d "$DIRECTORY_PROXY" ]; then
     echo "Directory $DIRECTORY_PROXY does not exist. Creating it now..."
@@ -7,13 +7,13 @@ if [ ! -d "$DIRECTORY_PROXY" ]; then
     mkdir "$DIRECTORY_PROXY"
 fi
 
-if [ ! -d "$DIRECTORY_NATIVE_LIBRARIES" ]; then
-    echo "Directory $DIRECTORY_NATIVE_LIBRARIES does not exist. Creating it now..."
-    git clone https://github.com/projectdana/native_libraries.git
-    mkdir "$DIRECTORY_NATIVE_LIBRARIES"
-fi
+# if [ ! -d "$DIRECTORY_NATIVE_LIBRARIES" ]; then
+#     echo "Directory $DIRECTORY_NATIVE_LIBRARIES does not exist. Creating it now..."
+#     git clone https://github.com/projectdana/native_libraries.git
+#     mkdir "$DIRECTORY_NATIVE_LIBRARIES"
+# fi
 
-cp custom/* native_libraries/
+# cp custom/* native_libraries/
 
 source ./scripts/k3s/build-publish-main.sh
 source ./scripts/k3s/build-publish-remote.sh
